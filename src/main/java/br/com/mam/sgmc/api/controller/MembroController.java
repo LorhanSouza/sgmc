0c76da3010633e589efee647e0e0bab27a4108ec
lorhansouza-wmtrading <lorhan.souza@wmtrading.com.br>
2026-05-15 15:05:46 -0300

    :hammer: reafactor: readaptar as respostas do endpoints do controlador de Membro

--- src/main/java/br/com/mam/sgmc/api/controller/MembroController.java
+++ src/main/java/br/com/mam/sgmc/api/controller/MembroController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.mam.sgmc.api.dto.request.MembroRequestDTO;
 import br.com.mam.sgmc.api.dto.response.MembroResponseDTO;
+import br.com.mam.sgmc.api.openapi.MembroControllerOpenAPI;
 import br.com.mam.sgmc.model.Membro;
 import br.com.mam.sgmc.services.MembroService;
 import jakarta.validation.Valid;
@@ -27,7 +29,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping(value = "/membros")
 @RequiredArgsConstructor
-public class MembroController {
+public class MembroController implements MembroControllerOpenAPI{
 
     private final MembroService membroService;
 
@@ -46,7 +48,6 @@ public class MembroController {
         membro.setAtivo(membroDTO.getCodigoAtivo());
         membro.setTamanhoCamisa(membroDTO.getTamanhoCamisa());
         membro.setDataAdmissao(Date.valueOf(membroDTO.getDataAdmissao()));
-
 
         membro = this.membroService.salvarMembro(membro, membroDTO.getIdCargo(), membroDTO.getIdSede());
 
@@ -71,7 +72,7 @@ public class MembroController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Membro> atualizarMembro(@PathVariable Long id, @RequestBody @Valid MembroRequestDTO membroDTO) {
+    public ResponseEntity<MembroResponseDTO> atualizarMembro(@PathVariable Long id, @RequestBody @Valid MembroRequestDTO membroDTO) {
         Membro membro = new Membro();
         membro.setNome(membroDTO.getNome());
         membro.setApelido(membroDTO.getApelido());
@@ -86,6 +87,12 @@ public class MembroController {
         membro.setDataAdmissao(Date.valueOf(membroDTO.getDataAdmissao()));
 
         membro = this.membroService.atualizarMembro(membro, id, membroDTO.getIdCargo(), membroDTO.getIdSede());
-        return ResponseEntity.status(HttpStatus.OK).body(membro);
+        return ResponseEntity.status(HttpStatus.OK).body(MembroResponseDTO.toResponseDTO(membro));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> inativarMembro(@PathVariable Long id) {
+        this.membroService.inativarMembro(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
