7f34a14796e180801fda5564701007e7dc9bf362
lorhansouza-wmtrading <lorhan.souza@wmtrading.com.br>
2026-05-19 15:40:20 -0300

    :hammer: refactor: adicionar anotações no DTO de request de `FichaMedica`

--- src/main/java/br/com/mam/sgmc/api/dto/request/FichaMedicaRequestDTO.java
+++ src/main/java/br/com/mam/sgmc/api/dto/request/FichaMedicaRequestDTO.java
@@ -1,9 +1,13 @@
 package br.com.mam.sgmc.api.dto.request;
 
 import br.com.mam.sgmc.model.FichaMedica;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FichaMedicaRequestDTO {
     private String nomePlanoSaude;
     private String numeroCarteira;
