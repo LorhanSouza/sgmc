a5cb7bc584ce2e5ca7e703a38501d1724ca41da2
lorhansouza-wmtrading <lorhan.souza@wmtrading.com.br>
2026-05-19 15:37:03 -0300

    :sparkles: feat: criar DTOs de `FichaMedica`

---
+++ src/main/java/br/com/mam/sgmc/api/dto/request/FichaMedicaRequestDTO.java
@@ -0,0 +1,27 @@
+package br.com.mam.sgmc.api.dto.request;
+
+import br.com.mam.sgmc.model.FichaMedica;
+import lombok.Data;
+
+@Data
+public class FichaMedicaRequestDTO {
+    private String nomePlanoSaude;
+    private String numeroCarteira;
+    private String tipoSanguineo;
+    private String alergias;
+    private String medicamentos;
+    private String condicoesMedicas;
+    private String observacoes;
+
+    public static FichaMedica paraEntidade(FichaMedicaRequestDTO requestDTO) {
+        FichaMedica fichaMedica = new FichaMedica();
+        fichaMedica.setNomePlanoSaude(requestDTO.getNomePlanoSaude());
+        fichaMedica.setNumeroCarteira(requestDTO.getNumeroCarteira());
+        fichaMedica.setTipoSanguineo(requestDTO.getTipoSanguineo());
+        fichaMedica.setAlergias(requestDTO.getAlergias());
+        fichaMedica.setMedicamentos(requestDTO.getMedicamentos());
+        fichaMedica.setCondicoesMedicas(requestDTO.getCondicoesMedicas());
+        fichaMedica.setObservacoes(requestDTO.getObservacoes());
+        return fichaMedica;
+    }
+}
